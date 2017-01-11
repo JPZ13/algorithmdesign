4e817656313b83f729442c95ee466ac5593dd9ed
JPZ13 <j.p.zivalich@gmail.com>
2017-01-10 23:03:09 -0800

    implement insertion sort in js

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const insertionSort = ( arr ) => {
-
+  for ( let i = 0; i < arr.length; i++ ) {
+    let j = i;
+    while ( j > 0 && arr[ j ] < arr[ j - 1 ] ) {
+      let swap = arr[ j - 1 ];
+      arr[ j - 1 ] = arr[ j ];
+      arr[ j ] = swap;
+      j = j - 1;
+    }
+  }
+  return arr;
 };
 
 module.exports = insertionSort;
