1394b61c2cb3b502cc815805915cad264d9e6a23
JPZ13 <j.p.zivalich@gmail.com>
2017-01-10 22:53:13 -0800

    clean filestructure

--- insertionSort/insertionSort.py
+++ insertionSort/insertionSort.py
@@ -1,2 +1,8 @@
 def insertion_sort( arr ):
-    return arr
+    for i in arr:
+        j = i
+        while j > 0 and arr[ j ] < arr[ j - 1 ]:
+            swap = arr[ j ]
+            arr[ j ] = arr[ j - 1 ]
+            arr[ j - 1 ] = swap
+            j = j - 1
