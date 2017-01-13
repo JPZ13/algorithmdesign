655fa5a645bab3953be1c4e5c7d130ec5e8ded65
JPZ13 <j.p.zivalich@gmail.com>
2017-01-12 22:12:41 -0800

    update spec - 7 length array

--- minimalTree/minimalTree.spec.js
+++ minimalTree/minimalTree.spec.js
@@ -16,7 +16,7 @@ var oneBST = {
   left: null,
   right: null,
   height: 0
-}
+};
 
 var threeBST = {
   value: 3,
@@ -24,13 +24,53 @@ var threeBST = {
   left: null,
   right: null,
   height: 0
-}
+};
+
+var fourBST = {
+  value: 4,
+  parent: null,
+  left: twoBST,
+  right: null,
+  height: 2
+};
+
+var fiveBST = {
+  value: 5,
+  parent: null,
+  left: null,
+  right: null,
+  height: 0
+};
+
+var sixBST = {
+  value: 6,
+  parent: fourBST,
+  left: fiveBST,
+  right: null,
+  height: 1
+};
+
+var sevenBST = {
+  value: 7,
+  parent: sixBST,
+  left: null,
+  right: null,
+  height: 0
+};
 
 twoBST.left = oneBST;
 twoBST.right = threeBST;
 
 describe( 'Minimal Tree', function() {
-  it( 'Should reuturn a BST for an ordered array with a length of 3', function() {
+  it( 'Should return a BST for an ordered array with a length of 3', function() {
     expect( minimalTree( [ 1, 2, 3 ] ) ).to.eql( twoBST );
+  });
+
+  it( 'Should return a BST for 7', function() {
+    twoBST.parent = fourBST;
+    fiveBST.parent = sixBST;
+    fourBST.right = sixBST;
+    sixBST.right = sevenBST;
+    expect( minimalTree( [ 1, 2, 3, 4, 5, 6, 7 ] ) ).to.eql( fourBST );
   });
 });
